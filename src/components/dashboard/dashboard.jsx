3f68de84822cbf3018210eb03bdc9db64d485dc0
Norris Kwan <norris.kwan@gmail.com>
2017-12-03 23:34:53 -0800

    Style changes

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,41 +1,39 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, {Component} from "react";
+import {connect} from "react-redux";
 import Add from "../add/add";
 import Pane from "../pane/pane";
 
+const mapStateToProps = (state, ownProps) => ({subscriptions: state.subreddits.subscriptions});
 
-const mapStateToProps = (state, ownProps) => ({
-  subscriptions: state.subreddits.subscriptions
-});
-
-const mapDispatchToProps = dispatch => ({
-});
+const mapDispatchToProps = dispatch => ({});
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { subscriptions: this.props.subscriptions };
-    this.renderPanes = this.renderPanes.bind(this);
+    this.state = {
+      subscriptions: this.props.subscriptions
+    };
+    this.renderPanes = this
+      .renderPanes
+      .bind(this);
   }
 
   componentWillReceiveProps(newProps) {
-    this.setState({
-      subscriptions: newProps.subscriptions,
-    });
+    this.setState({subscriptions: newProps.subscriptions});
   }
 
   renderPanes() {
-    return this.state.subscriptions.map((sub, i) => (
-      <Pane sub={sub} key={i} />
-    ));
+    return this
+      .state
+      .subscriptions
+      .map((sub, i) => (<Pane sub={sub} key={i}/>));
   }
 
   render() {
     return (
       <div className="dashboard">
-        <Add />
-        {this.renderPanes()}
+        <Add/> {this.renderPanes()}
       </div>
     );
   }
--- src/components/pane/pane.jsx
+++ src/components/pane/pane.jsx
@@ -1,53 +1,53 @@
 import React, {Component} from "react";
-import { connect } from "react-redux";
-import { fetchPosts } from "../../reducers/postsReducer";
+import {connect} from "react-redux";
+import {fetchPosts} from "../../reducers/postsReducer";
 
-const mapStateToProps = ({ postsBySubs }, ownProps) => {
-  return { postsBySubs };
-};
+const mapStateToProps = (state, ownProps) => ({postsBySubs: state.postsBySubs});
 
 const mapDispatchToProps = dispatch => ({
-  fetchPosts: sub => dispatch(fetchPosts(sub)),
+  fetchPosts: sub => dispatch(fetchPosts(sub))
 });
-
 
 class Pane extends Component {
   constructor(props) {
     super(props);
-    this.state = { chartType: '' };
+    this.state = {
+      chartType: ''
+    };
   }
 
   componentDidMount() {
-    console.log(this.props);
-    console.log('this.props.sub inside pane', this.props.sub);
     this
       .props
       .fetchPosts(this.props.sub);
   }
 
   renderPosts() {
-    const {sub, postsBySubs } = this.props;
-    const posts = !postsBySubs[sub] ? [] : Object.keys(postsBySubs[sub]);
+    const {sub, postsBySubs} = this.props;
+    const posts = !postsBySubs[sub]
+      ? []
+      : Object.keys(postsBySubs[sub]);
 
     return (
-      <ul>
-          {posts.map(id => {
-            return (
-              <li key={id}>
-                <strong>Title: </strong>{postsBySubs[sub][id].title}<br/>
-                <strong>Author: </strong>{postsBySubs[sub][id].author}
-              </li>
-            );
-          })}
-        </ul>
+      <ul className="posts">
+        {posts.map(id => {
+          return <li key={id}>
+              <strong>Title:</strong>
+              <span>{postsBySubs[sub][id].title}</span>
+              <br />
+              <strong>Author:</strong>
+              <span>{postsBySubs[sub][id].author}</span>
+            </li>;
+        })}
+      </ul>
     );
   }
 
   render() {
     return <div className="Pane">
-        <h1>{this.props.sub}</h1>
-        {this.renderPosts()}
-      </div>;
+      <h2>{this.props.sub}</h2>
+      {this.renderPosts()}
+    </div>;
   }
 }
 
